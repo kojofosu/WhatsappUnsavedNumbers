d2ea8227c98a9b661133df203abfe16f76d6d903
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2021-12-17 18:48:55 +0000

    converted project from Java to Kotlin

--- app/src/main/java/com/mcdev/whatsappunsavednumbers/MainActivity.kt
+++ app/src/main/java/com/mcdev/whatsappunsavednumbers/MainActivity.kt
@@ -1,59 +1,47 @@
-package com.mcdev.whatsappunsavednumbers;
+package com.mcdev.whatsappunsavednumbers
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import com.hbb20.CountryCodePicker
+import android.widget.EditText
+import android.os.Bundle
+import com.mcdev.whatsappunsavednumbers.R
+import android.view.WindowManager
+import android.widget.Toast
+import android.content.Intent
+import android.net.Uri
+import android.view.View
+import android.widget.Button
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.hbb20.CountryCodePicker;
-
-public class MainActivity extends AppCompatActivity {
-
-    CountryCodePicker countryCodePicker;
-    EditText inputNumberEt;
-    Button startChatBtn;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
-        init();
-
-
-
-        String countryCode = countryCodePicker.getSelectedCountryCodeWithPlus();
-
-        startChatBtn.setOnClickListener(view -> {
-            String phoneNumber = countryCode + inputNumberEt.getText().toString();
+class MainActivity : AppCompatActivity() {
+    var countryCodePicker: CountryCodePicker? = null
+    var inputNumberEt: EditText? = null
+    var startChatBtn: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        init()
+        val countryCode = countryCodePicker!!.selectedCountryCodeWithPlus
+        startChatBtn!!.setOnClickListener { view: View? ->
+            val phoneNumber = countryCode + inputNumberEt!!.text.toString()
             if (phoneNumber.isEmpty()) {
-                Toast.makeText(this, "Phone number cannot be empty.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Phone number cannot be empty.", Toast.LENGTH_SHORT).show()
             } else {
-                openWhatsApp(phoneNumber);
+                openWhatsApp(phoneNumber)
             }
-        });
-
+        }
     }
 
-    private void init() {
-        inputNumberEt = findViewById(R.id.etInputNumber);
-        startChatBtn = findViewById(R.id.btnStartChat);
-        countryCodePicker = findViewById(R.id.countryCodePicker);
+    private fun init() {
+        inputNumberEt = findViewById(R.id.etInputNumber)
+        startChatBtn = findViewById(R.id.btnStartChat)
+        countryCodePicker = findViewById(R.id.countryCodePicker)
     }
 
-    private void openWhatsApp(String phoneNumber) {
-        String url = getString(R.string.whatsapp_send_message_base_url) + phoneNumber;
-
-        Intent openWhatsAppIntent = new Intent(Intent.ACTION_VIEW);
-        openWhatsAppIntent.setData(Uri.parse(url));
-        startActivity(openWhatsAppIntent);
+    private fun openWhatsApp(phoneNumber: String) {
+        val url = getString(R.string.whatsapp_send_message_base_url) + phoneNumber
+        val openWhatsAppIntent = Intent(Intent.ACTION_VIEW)
+        openWhatsAppIntent.data = Uri.parse(url)
+        startActivity(openWhatsAppIntent)
     }
 }
