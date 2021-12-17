96af9cddf260eb7adecaf0346f0f45002b1c02e2
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2021-12-17 18:55:47 +0000

    Add view binding

--- app/src/main/java/com/mcdev/whatsappunsavednumbers/MainActivity.kt
+++ app/src/main/java/com/mcdev/whatsappunsavednumbers/MainActivity.kt
@@ -1,41 +1,32 @@
 package com.mcdev.whatsappunsavednumbers
 
 import androidx.appcompat.app.AppCompatActivity
-import com.hbb20.CountryCodePicker
-import android.widget.EditText
 import android.os.Bundle
-import com.mcdev.whatsappunsavednumbers.R
 import android.view.WindowManager
 import android.widget.Toast
 import android.content.Intent
 import android.net.Uri
-import android.view.View
-import android.widget.Button
+import android.view.LayoutInflater
+import com.mcdev.whatsappunsavednumbers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    var countryCodePicker: CountryCodePicker? = null
-    var inputNumberEt: EditText? = null
-    var startChatBtn: Button? = null
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        val root = binding.root
         window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-        init()
-        val countryCode = countryCodePicker!!.selectedCountryCodeWithPlus
-        startChatBtn!!.setOnClickListener { view: View? ->
-            val phoneNumber = countryCode + inputNumberEt!!.text.toString()
+
+        val countryCode = binding.countryCodePicker.selectedCountryCodeWithPlus
+        binding.btnStartChat.setOnClickListener {
+            val phoneNumber = countryCode + binding.etInputNumber.text.toString()
             if (phoneNumber.isEmpty()) {
                 Toast.makeText(this, "Phone number cannot be empty.", Toast.LENGTH_SHORT).show()
             } else {
                 openWhatsApp(phoneNumber)
             }
         }
-    }
-
-    private fun init() {
-        inputNumberEt = findViewById(R.id.etInputNumber)
-        startChatBtn = findViewById(R.id.btnStartChat)
-        countryCodePicker = findViewById(R.id.countryCodePicker)
     }
 
     private fun openWhatsApp(phoneNumber: String) {
