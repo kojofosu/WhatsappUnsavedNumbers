fc294ed5e851696723e4d1daca922f62c7882242
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2021-12-18 13:46:49 +0000

    ui changes

--- app/src/main/java/com/mcdev/whatsappunsavednumbers/MainActivity.kt
+++ app/src/main/java/com/mcdev/whatsappunsavednumbers/MainActivity.kt
@@ -6,22 +6,26 @@ import android.view.WindowManager
 import android.widget.Toast
 import android.content.Intent
 import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import com.mcdev.whatsappunsavednumbers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        binding = ActivityMainBinding.inflate(layoutInflater)
         val root = binding.root
-        window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        setContentView(root)
+        //window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
 
-        val countryCode = binding.countryCodePicker.selectedCountryCodeWithPlus
         binding.btnStartChat.setOnClickListener {
+            val countryCode = binding.countryCodePicker.selectedCountryCodeWithPlus
+            Log.d(TAG, "onCreate: country code is $countryCode")
             val phoneNumber = countryCode + binding.etInputNumber.text.toString()
-            if (phoneNumber.isEmpty()) {
+            if (phoneNumber.isEmpty() || phoneNumber.isBlank()) {
                 Toast.makeText(this, R.string.phone_number_cannot_be_null, Toast.LENGTH_SHORT).show()
             } else {
                 openWhatsApp(phoneNumber)
@@ -30,6 +34,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun openWhatsApp(phoneNumber: String) {
+        Log.d(TAG, "onCreate: Starting chat with $phoneNumber")
         val url = getString(R.string.whatsapp_send_message_base_url) + phoneNumber
         val openWhatsAppIntent = Intent(Intent.ACTION_VIEW)
         openWhatsAppIntent.data = Uri.parse(url)
