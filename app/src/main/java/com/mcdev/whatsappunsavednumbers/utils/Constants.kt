df111c86d35bf6a9669e6620ccfff5c0f098c6ac
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2022-03-18 15:41:19 +0000

    Fix WhatsApp copyright infringment

--- app/src/main/java/com/mcdev/whatsappunsavednumbers/utils/Constants.kt
+++
@@ -1,4 +0,0 @@
-package com.mcdev.whatsappunsavednumbers.utils
-
-const val BASE_URL = "https://api.whatsapp.com/send?phone="
-const val APP_VERSION = "v1.0"
---
+++ app/src/main/java/com/mcdev/wun/utils/Constants.kt
@@ -0,0 +1,4 @@
+package com.mcdev.wun.utils
+
+const val BASE_URL = "https://api.whatsapp.com/send?phone="
+const val APP_VERSION = "v1.0"
