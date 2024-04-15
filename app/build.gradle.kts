65dcaf20f9fb646427581f4be27855ffc31031e9
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2024-04-15 14:52:23 +0000

    working 1

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id  ("com.android.application")
+    id ("kotlin-android")
+}
+
+android {
+    namespace = "com.mcdev.wun"
+    defaultConfig {
+        minSdk = 23
+        targetSdk =34
+        compileSdk  =34
+        versionCode = 2
+        versionName = "1.1"
+        setProperty("archivesBaseName", "WUN-v${versionName}")
+
+        testInstrumentationRunner  = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles (getDefaultProguardFile ("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+}
+
+dependencies {
+    val lottieVersion = "6.4.0"
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation ("com.google.android.material:material:1.11.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    //country code picker library
+    implementation ("com.hbb20:ccp:2.7.0")
+    //Lottie
+    implementation ("com.airbnb.android:lottie:${lottieVersion}")
+    implementation ("androidx.core:core-ktx:1.12.0")
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.20")
+}
+repositories {
+    mavenCentral()
+}
--- app/src/main/java/com/mcdev/wun/utils/Constants.kt
+++ app/src/main/java/com/mcdev/wun/utils/Constants.kt
@@ -1,4 +1,3 @@
 package com.mcdev.wun.utils
 
 const val BASE_URL = "https://api.whatsapp.com/send?phone="
-const val APP_VERSION = "v1.0"
