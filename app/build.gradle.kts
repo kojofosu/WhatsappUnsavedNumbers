1eca487137575d7e7debb60b2d5453e846e65caf
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2026-06-15 07:37:49 +0000

    v2.0 compose release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         minSdk = 23
         targetSdk =34
         compileSdk  =34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "2.0"
         archivesName = "WUN-v${versionName}"
 
         testInstrumentationRunner  = "androidx.test.runner.AndroidJUnitRunner"
